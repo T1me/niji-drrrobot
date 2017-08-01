7c286bd1954e204a7a01d290ab4868a99e6a324c
時光會凝聚嗎 <willtimecondense@qq.com>
2017-08-01 08:15:07 -0500

    add tips feature

--- drrrobot.py
+++ drrrobot.py
@@ -3,6 +3,7 @@
 import time
 import requests
 import re
+import random
 
 
 class Bot(object):
@@ -82,7 +83,7 @@ class Bot(object):
                         list_id = re.findall('"id":".*?"', tu)
                         if len(list_id) > 2:
                             self.leave_room()
-                            quit()
+                            return 'leave'
                     elif '!room sd' in message:
                         list_id = re.findall('"id":".*?"', tu)
                         if len(list_id) > 2:
@@ -92,7 +93,33 @@ class Bot(object):
                 self.post('/me 歡迎光臨，輕食咖啡館')
             ru.close()
 
+    def give_time(self):
+        while 1:
+            if time.time() % 600 < 5:
+                try:
+                    give_time = time.strftime('現在是中原標準時間 %Y年%m月%d日 %H時%M分',time.localtime(time.time()))
+                    self.post('/me %s' % give_time)
+                    time.sleep(580)
+                except:
+                    print '[Err] Give time error at %s' % time.strftime('%Y/%m/%d %H:%M:%S',time.localtime(time.time()))
+
     def tips(self):
         while 1:
-            self.post('/me 歡迎光臨，輕食咖啡館')
-            time.sleep(1200)
+            time.sleep(7200 * random.random())
+            if 0 < int(time.strftime('%H',time.localtime(time.time()))) < 6:
+                list_tips = ['/me 寂静的深夜，除了自己的心跳，还有我在陪你',
+                             '/me 夜都睡了，你还不睡吗？',
+                             '/me 晚安，月亮也一起睡了',
+                             '/me 夜深了，天上的星星都已经睡了',
+                             '/me 晚睡伤身体，客官早点歇息吧',
+                             '/me 深夜了，不知道有多少人和你一样还没有睡呢？',
+                             '/me 夜深了，来日方长，早点休息',
+                             '/me 不要活得太累，不要忙得太疲惫',
+                             '/me 愿与您分享这夜色，早点休息',
+                             '/me 还在熬夜的您，让我为您点一盏灯',
+                             '/me 今夜微风轻送，伴你入美梦',
+                             '/me 周末也要早点休息，晚安地球人',
+                             '/me 夜深了，常常熬夜可不好哦'
+                ]
+                list_tips_index = int(13 * random.random())
+                self.post(list_tips[list_tips_index])
--- main.py
+++ main.py
@@ -5,12 +5,18 @@ import os
 import threading
 import time
 
-name = 'にじ'
+name = 'にじ#niji1997'
 icon = 'bakyura-2x'
 file_name = 'niji.cookie'
 url_room = raw_input('Input the [Room URL] ')
 
 niji = drrrobot.Bot(name=name,icon=icon)
+
+t_give_time = threading.Thread(target=niji.give_time)
+t_give_time.start()
+
+t_tips = threading.Thread(target=niji.tips)
+t_tips.start()
 
 while 1:
     try:
@@ -18,15 +24,15 @@ while 1:
             niji.login()
             niji.save_cookie(file_name=file_name)
             room = niji.room_enter(url_room=url_room)
-            t_tips = threading.Thread(target=niji.tips)
-            t_tips.start()
-            niji.room_update(room_text=room)
+            leave = niji.room_update(room_text=room)
+            if leave == 'leave':
+                break
         else:
             niji.load_cookie(file_name=file_name)
             room = niji.room_enter(url_room=url_room)
-            t_tips = threading.Thread(target=niji.tips)
-            t_tips.start()
-            niji.room_update(room_text=room)
+            leave = niji.room_update(room_text=room)
+            if leave == 'leave':
+                break
         time.sleep(10)
     except:
-        print '[Err] connection error'
+        print '[Err] Room update error at %s' % time.strftime('%Y/%m/%d %H:%M:%S',time.localtime(time.time()))
